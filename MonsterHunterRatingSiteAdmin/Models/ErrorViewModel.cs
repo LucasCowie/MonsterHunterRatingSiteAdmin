8af5cbc0c335cbf0d9c6269fe44bb26b8b2eb00f
Lucas <w0487255@nscc.ca>
2026-09-15 11:20:46 -0300

    Add project files.

---
+++ MonsterHunterRatingSiteAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MonsterHunterRatingSiteAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
